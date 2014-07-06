563adcfcd66446fd043229ecc1c3a2db6a2cc3a0
Fábio Lemos Elizandro <fabio@elizandro.com.br>
2014-07-06 16:02:50 -0300

    Primeiro commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anjinhospeludosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the anjinhospeludosApp
+ */
+angular.module('anjinhospeludosApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anjinhospeludosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the anjinhospeludosApp
+ */
+angular.module('anjinhospeludosApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
